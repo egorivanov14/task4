b06ca8565cbc6adc439ca341800580e87ac55409
Admin <max203383@gmail.com>
2026-04-22 17:14:02 +0300

    task 4

---
+++ src/main/java/com/innowise/web/config/PublicConstants.java
@@ -0,0 +1,14 @@
+package com.innowise.web.config;
+
+public class PublicConstants {
+  public static final String USER_PARAMETER = "user";
+  public static final String PASSWORD_PARAMETER = "password";
+  public static final String USER_NAME_PARAMETER = "username";
+  public static final String USER_NAME_SQL_PARAMETER = "user_name";
+  public static final String CURRENT_PAGE_PARAMETER = "current_page";
+  public static final String ERROR_MESSAGE_PARAMETER = "error_msg";
+  public static final String LOGIN_PAGE = "pages/login.jsp";
+  public static final String REGISTER_PAGE = "pages/register.jsp";
+  public static final String MAIN_PAGE = "pages/main.jsp";
+  public static final String ERROR_PAGE = "pages/error.jsp";
+}
